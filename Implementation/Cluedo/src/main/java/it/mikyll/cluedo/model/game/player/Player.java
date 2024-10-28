12a70b8415aa3476840047b68fb66bdbcd00e2bd
Michele Righi <righi.michy@gmail.com>
2024-10-29 00:19:12 +0100

    fix(model.game): update Player class

--- Implementation/Cluedo/src/main/java/it/mikyll/cluedo/model/game/player/Player.java
+++ Implementation/Cluedo/src/main/java/it/mikyll/cluedo/model/game/player/Player.java
@@ -1,38 +1,40 @@
 package it.mikyll.cluedo.model.game.player;
 
-import it.mikyll.cluedo.model.game.board.Position;
-import it.mikyll.cluedo.model.game.clues.Characters;
+import it.mikyll.cluedo.model.game.clues.Character;
 import it.mikyll.cluedo.model.game.clues.Clue;
 import it.mikyll.cluedo.model.networking.User;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
- * An User becomes a Player when the game starts.
+ * A User becomes a Player when the game starts.
  */
 public abstract class Player extends User {
 	
 	private int turn = -1;
-	private Characters character = null;
+	private Character character = null;
 	private int[] position;
-	private Set<Clue> clues;
+	private List<Clue> clues;
 	private String notebook = "";
 
 	public Player(User user) {
 		super(user.getUsername());
+		this.clues = new ArrayList<>();
 	}
 	public Player(String username) {
 		super(username);
+		this.clues = new ArrayList<>();
 	}
 
-	public Set<Clue> getClues() {return clues;}
-	public void setClues(Set<Clue> clues) {this.clues = clues;}
+	public List<Clue> getClues() {return clues;}
+	public void setClues(List<Clue> clues) {this.clues = clues;}
 	public String getNotebook() {return notebook;}
 	public void setNotebook(String notebook) {this.notebook = notebook;}
 	public int getTurn() {return turn;}
 	public void setTurn(int turn) {this.turn = turn;}
-	public Characters getCharacter() {return character;}
-	public void setCharacter(Characters character) {this.character = character;}
+	public Character getCharacter() {return character;}
+	public void setCharacter(Character character) {this.character = character;}
 	public int[] getPosition() {return position;}
 	public void setPosition(int[] position) {this.position = position;}
 
